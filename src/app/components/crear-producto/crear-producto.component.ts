8e581b9dbea12d24bc8496174697e538882b0e0e
Ana Paula Morales Dulzaides <anapaulamoralesdulzaidesrgmail.com>
2021-12-16 01:11:50 +0100

    createProducto

--- src/app/components/crear-producto/crear-producto.component.ts
+++ src/app/components/crear-producto/crear-producto.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { Producto } from 'src/app/models/producto';
+import { ProductoService } from 'src/app/services/producto.service';
 
 @Component({
   selector: 'app-crear-producto',
@@ -14,7 +15,8 @@ export class CrearProductoComponent implements OnInit {
   
   constructor(private fb:FormBuilder,
     private router:Router,
-    private toastr: ToastrService) { 
+    private toastr: ToastrService,
+    private productoService:ProductoService) { 
     this.productoForm=this.fb.group({
       producto:['',Validators.required],
       categoria:['',Validators.required],
@@ -26,14 +28,19 @@ export class CrearProductoComponent implements OnInit {
   ngOnInit(): void {
   }
   agregarProducto(){
-    const PRODUCTO:Producto={
+    const NEW_PRODUCTO:Producto={
       nombre:this.productoForm.get('producto')?.value,
       categoria:this.productoForm.get('categoria')?.value,
       ubicacion:this.productoForm.get('ubicacion')?.value,
       precio:this.productoForm.get('precio')?.value,
     }
-    console.log(PRODUCTO)
-    this.toastr.success('producto registrado', 'producto registrado');
-    this.router.navigate(['/'])
+    console.log(NEW_PRODUCTO)
+    this.productoService.createProducto(NEW_PRODUCTO).subscribe(data=>{
+      this.toastr.success('producto registrado', 'producto registrado');
+      this.router.navigate(['/'])
+    },error=>{
+      console.log(error)
+      this.productoForm.reset()
+    })
   }
 }
--- src/app/services/producto.service.ts
+++ src/app/services/producto.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Producto } from '../models/producto';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,8 @@ export class ProductoService {
   BASE_URL = 'http://localhost:4000/producto/'
   GET_ALL_PRODUCTOS=`${this.BASE_URL}allProductos`
   DELETE_PRODUCTO=`${this.BASE_URL}delete/`
+  CREATE_PRODUCTO=`${this.BASE_URL}create`
+  UPDATE_PRODUCTO=`${this.BASE_URL}update/`
 
   constructor(private httpClient: HttpClient) { }
 
@@ -18,5 +21,13 @@ export class ProductoService {
 
   deleteProducto(id:string): Observable<any>{
     return this.httpClient.delete(this.DELETE_PRODUCTO+id)
+  }
+
+  /* updateProducto(id:string): Observable<any>{
+    return this.httpClient.put(this.UPDATE_PRODUCT+id)
+  } */
+
+  createProducto(producto:Producto): Observable<any>{
+    return this.httpClient.post(this.CREATE_PRODUCTO,producto)
   }
 }
